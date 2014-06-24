7d9c070ccd1cd62718514ddc9fb9ded7f2fe9fc1
Mark Oberemk <moberemk@gmail.com>
2014-06-23 20:06:22 -0400

    Update loop.py
    Added additional comments to help the user better understand the complexities of Eric's code

--- loop.py
+++ loop.py
@@ -1,5 +1,10 @@
+# Initiate sum variable
 sum = 0
+# Generate a range of numbers from 1 to 11
 numbers = range(1,11)
+# Iterate over the numbers generated earlier
 for i in numbers:
+    # Add them to the sum
     sum += i
+# Print the sum out
 print(sum)
